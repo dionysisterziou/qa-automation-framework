f72548c309ba9f0df5dfe05f0319af19d0f4142d
Dionysis <dionysisterziou@gmail.com>
2026-02-26 20:49:33 +0200

    Common validation, many tests (no duplication)

--- Session_2/tests/test_payments.py
+++ Session_2/tests/test_payments.py
@@ -1,9 +1,11 @@
 import pytest
 from validations.payments import validate_payment
+from validations.payments import validate_payment_currency
 
 
 def test_payment_ok():
     payment = {"amount": 100, "status": "SUCCESS"}
+
     validate_payment(payment)
 
 
@@ -12,3 +14,10 @@ def test_payment_invalid_amount_should_fail():
 
     with pytest.raises(AssertionError):
         validate_payment(payment)
+
+
+def test_payment_currency():
+    payment = {"amount": 10, "status": "SUCCESS", "currency": "EUR"}
+
+    validate_payment(payment)
+    validate_payment_currency(payment)
--- Session_2/validations/payments.py
+++ Session_2/validations/payments.py
@@ -1,3 +1,7 @@
 def validate_payment(payment):
     assert payment["amount"] > 0, "Amount must be positive"
     assert payment["status"] == "SUCCESS", "Status must be SUCCESS"
+
+
+def validate_payment_currency(payment):
+    assert payment["currency"] == "EUR", "Currency must be EUR"
