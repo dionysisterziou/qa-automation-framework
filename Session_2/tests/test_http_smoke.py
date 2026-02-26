b8da9c5f1047533262fc1d8c8a27484ae3396b65
Dionysis <dionysisterziou@gmail.com>
2026-02-26 21:03:10 +0200

    Validate JSON body

--- Session_2/tests/test_http_smoke.py
+++ Session_2/tests/test_http_smoke.py
@@ -5,3 +5,11 @@ def test_get_post_1_status_code():
     response = requests.get("https://jsonplaceholder.typicode.com/posts/1")
 
     assert response.status_code == 200, "Status code should be 200"
+
+
+def test_get_post_1_body_fields():
+    response = requests.get("https://jsonplaceholder.typicode.com/posts/1")
+    data = response.json()
+
+    assert data["id"] == 1, "ID should be 1"
+    assert data["userId"] == 1, "User ID should be 1"
