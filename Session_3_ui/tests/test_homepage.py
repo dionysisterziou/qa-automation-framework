d50b0d398e47993450b431752c314b6a48b9f604
Dionysis <dionysisterziou@gmail.com>
2026-03-19 21:14:41 +0200

    Improve UI test stability by adding wait_for_url for redirect

--- Session_3_ui/tests/test_homepage.py
+++ Session_3_ui/tests/test_homepage.py
@@ -11,5 +11,6 @@ def test_homepage_has_expected_title(page: Page):
 def test_homepage_link_redirects(page: Page):
     page.goto("https://example.com")
     page.get_by_text("More information...").click()
+    page.wait_for_url("**iana.org**")
 
     assert "iana.org" in page.url
