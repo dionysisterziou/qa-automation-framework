5b3ce660a84b00dc5299a52070051a3aeacb1826
Dionysis <dionysisterziou@gmail.com>
2026-03-31 22:57:58 +0300

    Enhance navigation test with URL assertion and improved locator

--- Session_3_ui/tests/test_navigation.py
+++ Session_3_ui/tests/test_navigation.py
@@ -4,12 +4,12 @@ from test_data import LEARN_MORE_TEXT, IANA_HEADING
 
 
 @pytest.mark.parametrize(
-    "link_text, expected_heading",
+    "link_text, expected_url, expected_heading",
     [
-        (LEARN_MORE_TEXT, IANA_HEADING)
+        (LEARN_MORE_TEXT, "https://www.iana.org/help/example-domains", IANA_HEADING)
     ]
 )
-def test_learn_more_link_redirects_to_iana(homepage: Page, link_text, expected_heading):
-    homepage.get_by_text(link_text).click()
-    homepage.wait_for_url("**iana.org**")
+def test_learn_more_link_redirects_to_iana(homepage: Page, link_text, expected_url, expected_heading):
+    homepage.get_by_role("link", name=link_text).click()
+    expect(homepage).to_have_url(expected_url)
     expect(homepage.get_by_role("heading", name=expected_heading)).to_be_visible()
