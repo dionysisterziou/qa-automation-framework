d1562236526a62ed32f04800ab5475a6d05d1c16
Dionysis <dionysisterziou@gmail.com>
2026-06-09 15:34:08 +0300

    Improve UI navigation test data reuse

--- Session_3_ui/test_data.py
+++ Session_3_ui/test_data.py
@@ -1,3 +1,4 @@
 LEARN_MORE_TEXT = "Learn more"
 IANA_HEADING = "Example Domains"
 HOMEPAGE_HEADING = "Example Domain"
+IANA_URL = "https://www.iana.org/help/example-domains"
--- Session_3_ui/tests/test_navigation.py
+++ Session_3_ui/tests/test_navigation.py
@@ -1,13 +1,13 @@
 import pytest
 from playwright.sync_api import Page, expect
-from Session_3_ui.test_data import LEARN_MORE_TEXT, IANA_HEADING, HOMEPAGE_HEADING
+from Session_3_ui.test_data import LEARN_MORE_TEXT, IANA_HEADING, HOMEPAGE_HEADING, IANA_URL
 from Session_3_ui.config import BASE_URL
 
 
 @pytest.mark.parametrize(
     "link_text, expected_url, expected_heading",
     [
-        (LEARN_MORE_TEXT, "https://www.iana.org/help/example-domains", IANA_HEADING)
+        (LEARN_MORE_TEXT, IANA_URL, IANA_HEADING)
     ]
 )
 def test_learn_more_link_redirects_to_iana(homepage: Page, link_text, expected_url, expected_heading):
@@ -18,14 +18,9 @@ def test_learn_more_link_redirects_to_iana(homepage: Page, link_text, expected_u
 
 def test_user_can_return_to_homepage_after_learn_more_navigation(homepage: Page):
     learn_more_link = homepage.get_by_role("link", name=LEARN_MORE_TEXT)
-
     expect(learn_more_link).to_be_visible()
-
     learn_more_link.click()
-
-    expect(homepage).to_have_url("https://www.iana.org/help/example-domains")
-
+    expect(homepage).to_have_url(IANA_URL)
     homepage.go_back()
-
     expect(homepage).to_have_url(BASE_URL)
     expect(homepage.get_by_role("heading", name=HOMEPAGE_HEADING)).to_be_visible()
